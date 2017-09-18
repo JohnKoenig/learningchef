7b6a8cd6c0f329aedc1b015a032d34566bab0e35
root <root@ip-172-31-10-165.us-west-1.compute.internal>
2017-09-18 23:34:52 +0000

    update to file objects

--- cookbooks/bind/recipes/default.rb
+++ cookbooks/bind/recipes/default.rb
@@ -5,38 +5,30 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
 #
-yum -y update
+execute 'linux update' do
+	command 'yum -y update'
+	command 'yum -y install bind-chroot'
+	command 'yum -y install bind-utils'
+end
 
 package 'bind-chroot' do
 	package_name 'named-chroot'
 	action :install
 end
 
-yum -y install bind-chroot
-yum -y install bind-utils
-
-#/usr/libexec/setup-named-chroot.sh /var/named/chroot on
-
-#systemctl stop named
-#systemctl disable named
+execute 'named-chroot setup' do
+	command '/usr/libexec/setup-named-chroot.sh /var/named/chroot on'
+end
 
 service 'bind' do
         service_name 'bind'
         action [:stop, :disable]
 end
 
-#systemctl start named-chroot
-#systemctl enable named-chroot
-
 service 'dns' do
         service_name 'named-chroot'
         action [:start, :enable]
 end
-
-#touch /var/named/chroot/var/named/data/cache_dump.db
-#touch /var/named/chroot/var/named/data/named_stats.txt
-#touch /var/named/chroot/var/named/data/named_mem_stats.txt
-#touch /var/named/chroot/var/named/data/named.run
 
 execute 'initiate files' do
 	command 'touch /var/named/chroot/var/named/data/cache_dump.db'
@@ -45,18 +37,12 @@ execute 'initiate files' do
 	command 'touch /var/named/chroot/var/named/data/named.run'
 end
 
-#chown named.named /var/named/chroot/var/named/data/ -R
-#chmod -R 760 /var/named/chroot/var/named/data
-
 directory '/var/named/chroot/var/named/data/' do
 	action :create
 	mode '0760'
 	owner 'named'
 	group 'named'
 end
-
-
-#chmod -R 760 /var/named/chroot/var/named/dynamic
 
 directory '/var/named/chroot/var/named/dynamic' do
 	action :create
@@ -65,27 +51,22 @@ directory '/var/named/chroot/var/named/dynamic' do
 	group 'named'
 end
 
-
-#create /var/named/chroot/var/named/data/kemi.com.db
-
-file '/var/named/chroot/var/named/data/kemi.com.db' do
+#file '/var/named/chroot/var/named/data/kemi.com.db' do
         #create /var/named/chroot/var/named/data/reversezones.db
-end
+#end
 
-file '/var/named/chroot/var/named/data/10.1.10.db' do
+#file '/var/named/chroot/var/named/data/10.1.10.db' do
         #create /var/named/chroot/var/named/data/10.1.10.db
-end
+#end
 
-file '/var/named/chroot/var/named/data/10.1.20.db' do
+#file '/var/named/chroot/var/named/data/10.1.20.db' do
 	#create /var/named/chroot/var/named/data/10.1.20.db
-end
+#end
 
-file '/var/named/chroot/var/named.config' do # /etc/named.config
+#file '/var/named/chroot/var/named.config' do # /etc/named.config
 	#content 'something'
-end
+#end
 
-#vim /etc/resolv.conf (update search namespace and nameservers)
-
-file '/etc/resolv.conf' do
+#file '/etc/resolv.conf' do
         #content 'something'
-end
+#end
